78e2fd0a0d96610a5954b2dc6e53b2baadf86863
Alexander <alaax.aca@gmail.com>
2019-09-16 10:24:11 +0200

    Initial project push with application shell

---
+++ src/SolarApp.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SolarApp extends Application {
+
+    // JavaFX start method
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Creating the group (temp)
+        Group group;
+        group = new Group();
+
+        // Creating the scene (temp)
+        Scene scene;
+        scene = new Scene(group, 500, 500);
+
+        // Creating the stage (temp)
+        stage.setScene(scene);
+        stage.setTitle("Solar Calculation App");
+        stage.show();
+    }
+}
