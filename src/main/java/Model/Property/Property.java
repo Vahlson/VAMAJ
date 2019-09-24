1db5637ebc30bea6bd368e12cc81e0ba67ab5bdf
jutb <bunis@student.chalmers.se>
2019-09-24 17:50:31 +0200

    Added attributes needed UserStory 2 in Property Class

--- src/main/java/Model/Property/Property.java
+++ src/main/java/Model/Property/Property.java
@@ -7,6 +7,9 @@ public class Property {
     // Member variables
     private Location location;
     private Contract contract;
+    private double availableSpace;
+    private int orientation;
+    private int angle;
 
     // Constructor (empty)
     public Property() {
@@ -20,4 +23,17 @@ public class Property {
     public Contract getContract() {
         return contract;
     }
+
+    public double getInstallationSpace() { return availableSpace; }
+
+    public int getOrientation() { return orientation; }
+
+    public int getPanelAngle() { return angle; }
+
+    // Setters
+    public void setInstallationSpace(double installationSpace) { this.availableSpace = installationSpace; }
+
+    public void setOrientation(int orientation) { this.orientation = orientation; }
+
+    public void setPanelAngle(int panelAngle) { this.angle = panelAngle; }
 }
