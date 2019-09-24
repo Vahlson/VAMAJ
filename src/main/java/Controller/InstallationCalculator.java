71281c6514a6a2762a479dc1e2e9b5d09507b610
Alexander <alaax.aca@gmail.com>
2019-09-24 19:54:15 +0200

    Updated controller class

--- src/main/java/Controller/InstallationCalculator.java
+++ src/main/java/Controller/InstallationCalculator.java
@@ -1,14 +1,47 @@
 package main.java.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InstallationCalculator implements Initializable {
-    
+
+    // Components in parent FXML-file
+    @FXML
+    private TextField solarAreaField;
+
+    @FXML
+    private TextField costField;
+
+    @FXML
+    private Button calculateButton;
+
+    @FXML
+    private Text resultText;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+        // Initializing button functionality
+        calculateButton.setOnAction(event -> {
+
+            double area = Double.parseDouble(solarAreaField.getText());
+            double cost = Double.parseDouble(costField.getText());
+
+            resultText.setText("Kostnad: " + mockResult(area, cost));
+        });
+    }
+
+    // Just temporary helper method to calculate the costs, in future this will be done in calculator classes
+    private double mockResult(double area, double cost) {
+
+        // Simplified calculations for now
+        return area * cost;
     }
 }
