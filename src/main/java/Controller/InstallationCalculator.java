42359502409c8ad33ddf8364d211e3139beafdfa
Alexander <alaax.aca@gmail.com>
2019-09-24 20:04:16 +0200

    Made sure only numbers can be entered into text fields

--- src/main/java/Controller/InstallationCalculator.java
+++ src/main/java/Controller/InstallationCalculator.java
@@ -1,5 +1,7 @@
 package main.java.Controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -31,19 +33,21 @@ public class InstallationCalculator implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
 
+        // Initializing the text fields
+        onlyNumbers(solarAreaField);
+        onlyNumbers(costField);
+
         // Initializing button functionality
         calculateButton.setOnAction(event -> {
 
             double area = Double.parseDouble(solarAreaField.getText());
             double cost = Double.parseDouble(costField.getText());
 
-            resultText.setText("Kostnad: " + mockResult(area, cost));
+            resultText.setText("Kostnad: " + mockResult(area, cost) + " kr");
         });
 
         // Initializing back button functionality
-        backButton.setOnAction(event -> {
-            SceneSwitcher.getInstance().setScene("/main/java/View/resources/fxml/main.fxml");
-        });
+        backButton.setOnAction(event -> SceneSwitcher.getInstance().setScene("/main/java/View/resources/fxml/main.fxml"));
     }
 
     // Just temporary helper method to calculate the costs, in future this will be done in calculator classes
@@ -51,5 +55,14 @@ public class InstallationCalculator implements Initializable {
 
         // Simplified calculations for now
         return area * cost;
+    }
+
+    // Method that configures text field to only accept numbers
+    private void onlyNumbers(TextField tf) {
+        tf.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tf.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
     }
 }
