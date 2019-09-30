655c83c509f712477581560e38aaee96012695a9
Vegard <vegard.landro1@gmail.com>
2019-09-30 09:45:14 +0200

    Created class for annualSolarElectricity input

---
+++ src/main/java/model/Calculator/AnnualSolarElectricityOutput.java
@@ -0,0 +1,16 @@
+package main.java.model.Calculator;
+
+public class AnnualSolarElectricityOutput {
+    private double annualElectricityProduction;
+
+    public AnnualSolarElectricityOutput() {
+    }
+
+    public double getAnnualElectricityProduction() {
+        return annualElectricityProduction;
+    }
+
+    public void setAnnualElectricityProduction(double annualElectricityProduction) {
+        this.annualElectricityProduction = annualElectricityProduction;
+    }
+}
