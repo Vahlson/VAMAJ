c1d879f29468a4c9f2c04700446ebcac63d891db
Vegard <vegard.landro1@gmail.com>
2019-09-30 10:08:26 +0200

    minor fix, effiency percentage to decimal, right file this time

--- src/main/java/model/Calculator/AnnualSolarElectricity.java
+++ src/main/java/model/Calculator/AnnualSolarElectricity.java
@@ -11,7 +11,7 @@ public class AnnualSolarElectricity implements Calculator<AnnualSolarElectricity
         double latitude                 = input.getLatitude();
         double solarArea                = input.getSolarPVPArea();
         double solarEfficiency          = input.getSolarPVEfficiency();
-        double kWhPerSqMeterPerDay      = solarEfficiency * Insolation(latitude);
+        double kWhPerSqMeterPerDay      = (solarEfficiency/100) * Insolation(latitude);
         double kWhPerSqMeterPerYear     = kWhPerSqMeterPerDay * 365;
         double kWhPerYearTotal          = solarArea * kWhPerSqMeterPerYear;
 
