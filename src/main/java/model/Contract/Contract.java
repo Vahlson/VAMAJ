3625ff4774977a7238d0c329b320fffb474c6647
MIDO-96 <mohammadmulhem96@gmail.com>
2019-10-01 10:35:23 +0200

    contract.

--- src/main/java/model/Contract/Contract.java
+++ src/main/java/model/Contract/Contract.java
@@ -1,4 +1,49 @@
 package main.java.model.Contract;
 
 public class Contract {
+
+    private String contractProvider;
+    private String contractCity;
+    private double tariff;
+    private double contractDuration;
+
+
+
+    public Contract(String contractProvider, String contractCity, double tariff, double contractDuration) {
+        this.contractProvider = contractProvider;
+        this.contractCity = contractCity;
+        this.tariff = tariff;
+        this.contractDuration= contractDuration;
+    }
+
+    public String getContractProvider() {
+        return contractProvider;
+    }
+
+    public void setContractProvider(String contractProvider) {
+        this.contractProvider = contractProvider;
+    }
+
+    public String getContractCity() {
+        return contractCity;
+    }
+
+    public void setContractCity(String contractCity) {
+        this.contractCity = contractCity;
+    }
+
+    public double getTariff() {
+        return tariff;
+    }
+
+    public void setTariff(double tariff) {
+        this.tariff = tariff;
+    }
+    public double getContractDuration() {
+        return contractDuration;
+    }
+
+    public void setContractDuration(double contractDuration) {
+        this.contractDuration = contractDuration;
+    }
 }
