b59686eac2ca3bde0053668094f8a89bd9dc6f61
Legetic <Legetic@hotmail.com>
2019-10-01 11:05:35 +0200

    Added more tests for service package

--- src/main/java/model/Contract/Contract.java
+++ src/main/java/model/Contract/Contract.java
@@ -3,6 +3,10 @@ package main.java.model.Contract;
 public class Contract {
     private int cost;
 
+    public int getCost(){
+
+        return this.cost;
+    }
     public void setCost(int cost){
         this.cost = cost;
     }
--- test/ServicesTest.java
+++ test/ServicesTest.java
@@ -1,4 +1,5 @@
 import junit.framework.TestCase;
+import main.java.model.Contract.Contract;
 import main.java.model.Property.Location;
 import main.java.services.ServiceFacade;
 import org.junit.Test;
@@ -13,6 +14,14 @@ public class ServicesTest extends TestCase {
         assertEquals(l.getCoordinate().getX(), 10.5);
         assertEquals(l.getCoordinate().getY(), 25.3);
         assertEquals(l.getSolarPower(),1001);
+
+    }
+    @Test
+    public void testContractCreationFromAPI(){
+        ServiceFacade sf = new ServiceFacade();
+        Contract c = sf.getContract();
+        assertEquals(c.getCost(), 999);
+
 
     }
 
