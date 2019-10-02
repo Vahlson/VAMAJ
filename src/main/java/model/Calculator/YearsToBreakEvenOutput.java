35273d1c4886c5c8026d63f758fd77985ee61e69
Vegard <vegard.landro1@gmail.com>
2019-10-02 11:13:12 +0200

    Created class for YearsToBreakEven output

---
+++ src/main/java/model/Calculator/YearsToBreakEvenOutput.java
@@ -0,0 +1,13 @@
+package main.java.model.Calculator;
+
+public class YearsToBreakEvenOutput{
+    private int years;
+
+    YearsToBreakEvenOutput(int years) {
+        this.years = years;
+    }
+
+    public int getYears() {
+        return years;
+    }
+}
