1531977cec7ca6f26502b733f48c5c918cda1381
Vegard <vegard.landro1@gmail.com>
2019-10-02 11:14:45 +0200

    Created class for LevelizedCostOfElectricity output

---
+++ src/main/java/model/Calculator/LevelizedCostOfElectricityOutput.java
@@ -0,0 +1,13 @@
+package main.java.model.Calculator;
+
+public class LevelizedCostOfElectricityOutput {
+    private double lCOE;
+
+    LevelizedCostOfElectricityOutput(double lCOE) {
+        this.lCOE = lCOE;
+    }
+
+    public double getlCOE() {
+        return lCOE;
+    }
+}
