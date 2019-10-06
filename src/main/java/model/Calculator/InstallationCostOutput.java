c103af2a8c4c19b0db850c150bb3d0670b2336ae
jutb <bunis@student.chalmers.se>
2019-10-06 17:17:49 +0200

    Created class for InstallationCost output

---
+++ src/main/java/model/Calculator/InstallationCostOutput.java
@@ -0,0 +1,15 @@
+package main.java.model.Calculator;
+
+public class InstallationCostOutput {
+
+    private double installationCost;
+
+    public InstallationCostOutput(double installationCost) {
+        this.installationCost = installationCost;
+    }
+
+    //Getters
+    public double getInstallationCost() {
+        return installationCost;
+    }
+}
