89e0394590c34a1c47d58325f6aede18af68f999
Alexander <alaax.aca@gmail.com>
2019-10-06 19:47:52 +0200

    Refactored model classes in order to remove setter chain

--- src/main/java/model/ModelAggregate.java
+++ src/main/java/model/ModelAggregate.java
@@ -1,5 +1,6 @@
 package main.java.model;
 
+import main.java.model.Property.Property;
 import main.java.model.Calculator.Calculator;
 import main.java.model.Contract.Contract;
 import main.java.model.Property.Location;
@@ -12,12 +13,14 @@ public class ModelAggregate {
     private Calculator calculator;
     private User currentUser;
     private List<User> users;
+    private Property property;
+
 
     void setLocation(Location l) {
-        currentUser.setLocation(l);
+        property.setLocation(l);
     }
 
     void setContract(Contract c) {
-        currentUser.setContract(c);
+        property.setContract(c);
     }
 }
--- src/main/java/model/ModelFacade.java
+++ src/main/java/model/ModelFacade.java
@@ -5,7 +5,7 @@ import main.java.model.Property.Location;
 
 public class ModelFacade {
 
-    private ModelAggregate modelAggregate;
+    private main.java.model.ModelAggregate modelAggregate;
 
     public void setLocation(Location l) {
         modelAggregate.setLocation(l);
