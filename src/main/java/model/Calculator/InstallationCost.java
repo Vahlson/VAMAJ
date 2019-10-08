ad7bb4bddaf3b7086aeda5783e3122e2d0fa585d
jutb <bunis@student.chalmers.se>
2019-10-08 11:02:10 +0200

    Updated InstallationCost calculation according to UserStory7

--- src/main/java/model/Calculator/InstallationCost.java
+++ src/main/java/model/Calculator/InstallationCost.java
@@ -10,8 +10,28 @@ public class InstallationCost implements Calculator<InstallationCostInput, Insta
         double requiredPanelSpace   = input.getRequiredSpace();
         double panelPrice           = input.getPanelPrice();
 
-        double installationCost     = availableSpace / requiredPanelSpace * panelPrice;
+        double installationCost     = installSetupCost(input.getAvailableSpace(), input.getRequiredSpace(), input.getPanelPrice());
+        double governmentSubvention = subventionAmount(installationCost);
+        double subventedInstallationCost = subventedCost(installationCost);
 
-        return new InstallationCostOutput(installationCost);
+        return new InstallationCostOutput(installationCost, governmentSubvention, subventedInstallationCost);
+    }
+
+    // Calculates installationCost based on how many solar panels fits in availableSpace
+    private double installSetupCost(double availableSpace, double requiredPanelSpace, double panelPrice){
+
+        double installationCost = Math.floorDiv((int) availableSpace, (int) requiredPanelSpace) * panelPrice;
+
+        return installationCost;
+    }
+
+    // Returns the subvented amount based on installationCost
+    private double subventionAmount(double installationCost){
+        return installationCost * 0.2;
+    }
+
+    // Returns installationCost after subvention
+    private double subventedCost(double installationCost){
+        return installationCost * 0.8;
     }
 }
--- src/main/java/model/Calculator/InstallationCostOutput.java
+++ src/main/java/model/Calculator/InstallationCostOutput.java
@@ -5,13 +5,21 @@ public class InstallationCostOutput {
     // Class to contain return values of InstallationCost calculation
 
     private double installationCost;
+    private double governmentSubvention;
+    private double subventedCost;
 
-    public InstallationCostOutput(double installationCost) {
+    public InstallationCostOutput(double installationCost, double governmentSubvention, double subventedCost) {
         this.installationCost = installationCost;
+        this.governmentSubvention = governmentSubvention;
+        this.subventedCost = subventedCost;
     }
 
     //Getters
     public double getInstallationCost() {
         return installationCost;
     }
+
+    public double getGovernmentSubvention() { return governmentSubvention; }
+
+    public double getSubventedCost() { return subventedCost; }
 }
