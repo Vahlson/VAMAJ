18cf28a3111ebccfd3e7524a8e4849968fe15404
Alexander <alaax.aca@gmail.com>
2019-10-09 18:50:39 +0200

    Added test

--- src/test/java/GeolocationTest.java
+++ src/test/java/GeolocationTest.java
@@ -6,13 +6,11 @@ import services.Geolocation.Geolocation;
 public class GeolocationTest extends TestCase {
 
     @Test
-    public void test() {
+    public void testGeolocation() {
 
         Geolocation location;
         location = new Geolocation();
 
-        System.out.println(location.getCity());
-        System.out.println(location.getLatitude());
-        System.out.println(location.getLongitude());
+        assertEquals(location.getCity(), "Gothenburg");
     }
 }
