8b9918376dc62e88e1e5435fd1a2260e3fa33e2f
Alexander <alaax.aca@gmail.com>
2019-10-09 11:25:14 +0200

    Updated geolocation contract class

--- src/main/java/services/Geolocation/GeolocationService.java
+++ src/main/java/services/Geolocation/GeolocationService.java
@@ -1,7 +1,44 @@
 package services.Geolocation;
 
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.DomainResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 // Class that fetches geolocation data using our API:s
 public class GeolocationService {
 
-    // Member variables
+    // Creating the database reader
+    private DatabaseReader dbReader;
+
+    // Constructor (empty)
+    public GeolocationService() throws IOException {
+
+        // Loading the database
+        File database;
+        database = new File("");
+
+        // Instantiating the database reader
+        dbReader = new DatabaseReader.Builder(database).build();
+    }
+
+    public DomainResponse data(String ip) throws IOException, GeoIp2Exception {
+
+        ip = "128.101.101.101";
+
+        // Creating the ip address
+        InetAddress ipAddress;
+        ipAddress = InetAddress.getByName(ip);
+
+        // Getting the location data from the ip address
+        DomainResponse result;
+        result = dbReader.domain(ipAddress);
+
+        // Returning the result
+        return result;
+    }
 }
