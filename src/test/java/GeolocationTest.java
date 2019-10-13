9967870398b65bf5f8e6fa4fff1ef05a19f85db7
MhdJamal <45226938+MhdJamal@users.noreply.github.com>
2019-10-13 18:05:11 +0200

    Add files via upload

--- src/test/java/GeolocationTest.java
+++ src/test/java/GeolocationTest.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 
 public class GeolocationTest extends TestCase {
 
-    /*
+
     @Test
     public void testGeolocation() {
 
-        Geolocation location;
-        location = new Geolocation();
+      //  Geolocation location;
+        // location = new Geolocation();
 
-        assertEquals(location.getCity(), "Gothenburg");
-    }*/
+       // assertEquals(location.getCity(), "Gothenburg");
+        System.out.println("Geolocation class doesnt exist in Geolocation package");
+    }
 }
