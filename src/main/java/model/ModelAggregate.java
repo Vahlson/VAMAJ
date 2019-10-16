43fe2d35af6ad5c1ab6ab33e208ca7a9b7a52481
Vegard <vegard.landro1@gmail.com>
2019-10-16 18:03:23 +0200

    Changed aggregator to communicate with new Calculator structure

--- src/main/java/model/ModelAggregate.java
+++ src/main/java/model/ModelAggregate.java
@@ -1,6 +1,6 @@
 package main.java.model;
 
-import main.java.model.Calculator.*;
+import main.java.model.Calculators.*;
 import main.java.model.Property.Property;
 import main.java.model.Contract.Contract;
 import main.java.model.Property.Location;
@@ -15,62 +15,32 @@ import java.util.HashMap;
 
 public class ModelAggregate {
 
-    private Calculator<AnnualSolarElectricityInput, AnnualSolarElectricityOutput> AnnualCal = new AnnualSolarElectricity();
-
     private User currentUser;
     private List<User> users = new ArrayList<>();
     private Property property = new Property();
     private Location location = new Location();
-    private HashMap<String, Object> calculationResults = new HashMap<>(); //HashMap containing results with a string as key
+    private Double result; //HashMap containing values with a ENUM as key
+    private CalculatorRunner runner = new CalculatorRunner();
+    private CalculatorData localData;
+
 
     //Calls calculators and sets results to above HashMap
     void runCalculators() {
+        CalculatorData data = new CalculatorData();
 
-        // Calculating annual electricity output
-        double annualElectricityOutput = annualElectricityOutput(AnnualCal, property);
-        calculationResults.put("annualElectricity", annualElectricityOutput);
+        data.addValue(DataKey.CONSUMED_ELECTRICITY, 3.0);
+        data.addValue(DataKey.PRODUCED_ELECTRICITY, 6.0);
+        data.addValue(DataKey.AVAILABLE_SPACE, 3.0);
+        data.addValue(DataKey.REQUIRED_PANEL_SPACE, 6.0);
+        data.addValue(DataKey.PANEL_PRICE, 3.0);
+        data.addValue(DataKey.EXPECTED_LIFESPAN, 3.0);
+        data.addValue(DataKey.ANNUAL_OPERATION_COST, 6.0);
+        data.addValue(DataKey.ANNUAL_ELECTRICITY_PRODUCTION, 3.0);
 
-        // Calculating solar panel production
-        double solarPanelProduction = panelProductionOutput();
-        calculationResults.put("panelProduction", solarPanelProduction);
+        runner.calculateAll(data);
+        localData = data;
     }
 
-    //Sends values from property as calculation parameters, returns result of the calculation
-    private double annualElectricityOutput(Calculator<AnnualSolarElectricityInput, AnnualSolarElectricityOutput> calc, Property p) {
-        double lat = p.getLatitude();
-        double area = p.getInstallationSpace();
-        double efficiency = 22;
-
-        //Calculator<AnnualSolarElectricityInput, AnnualSolarElectricityOutput> AnnualCal;
-        AnnualSolarElectricityInput in = new AnnualSolarElectricityInput(lat, area, efficiency);
-        AnnualSolarElectricityOutput out = calc.calculate(in);
-        return out.getAnnualElectricityProduction();
-    }
-
-    // Method calculating the production output of solar panel based on given values (kW/h)
-    private double panelProductionOutput() {
-
-        // Creating the input (hard coded values for now)
-        SolarPanelProductionInput input;
-        input = new SolarPanelProductionInput();
-        input.setPanelPerformanceRatio(0.75); // Default value
-        input.setAverageSolarRadiation(2.5);
-        input.setPanelEfficiency(0.4);
-        input.setPanelArea(10);
-
-        // property.getSolarSetup().getEfficiency()
-        // property.getSolarSetup().getSpaceRequired()
-
-        SolarPanelProduction calculator;
-        calculator = new SolarPanelProduction();
-
-        // Creating the output
-        SolarPanelProductionOutput output;
-        output = calculator.calculate(input);
-
-        // Returning the result
-        return output.getEnergy();
-    }
 
     //Temporary setter for testing purposes
     public void setProperty(Property property) {
@@ -78,8 +48,8 @@ public class ModelAggregate {
     }
 
     // Getter for results of calculation(s)
-    public HashMap<String, Object> getCalculationResults() {
-        return calculationResults;
+    public Double getCalculationResult(DataKey key) {
+        return localData.getValue(key);
     }
 
     // Getters
