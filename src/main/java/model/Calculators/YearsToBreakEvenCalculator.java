97a721a60a0d5dcea08a897727bf9937117a33c5
Vegard <vegard.landro1@gmail.com>
2019-10-16 18:02:02 +0200

    Changed calculator for new structure

---
+++ src/main/java/model/Calculators/YearsToBreakEvenCalculator.java
@@ -0,0 +1,36 @@
+package main.java.model.Calculators;
+
+//(1.1) Vegard
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static main.java.model.Calculators.DataKey.*;
+
+public class YearsToBreakEvenCalculator implements Calculator{
+
+    private final Set<DataKey> requiredInput = new HashSet<>(Arrays.asList(INSTALLATION_COST, ANNUAL_OPERATION_COST, ANNUAL_ELECTRICITY_PRODUCTION, IMPORTED_ELECTRICITY_PRICE_KWH));
+
+    YearsToBreakEvenCalculator() {
+    }
+
+    //Returns approximate time to break-even on costs for a electricity generation setup
+    public void calculate(CalculatorData input){
+
+        double installationCost         = input.getValue(INSTALLATION_COST);
+        double annualOperationCost      = input.getValue(ANNUAL_OPERATION_COST);
+        double annualProduction         = input.getValue(ANNUAL_ELECTRICITY_PRODUCTION);
+        double electricityPricePerKWh   = input.getValue(IMPORTED_ELECTRICITY_PRICE_KWH);
+
+        double savingsPerYear = annualProduction * electricityPricePerKWh - annualOperationCost;
+        double yearsToPayInstallationCost = installationCost/savingsPerYear;
+
+        input.addValue(YEARS_TO_BREAK_EVEN, yearsToPayInstallationCost);
+    }
+
+    @Override
+    public Set<DataKey> getRequiredInput() {
+        return requiredInput;
+    }
+}
