96eb435e0eaaf34b5209cb3c462cef694c04f65f
jutb <bunis@student.chalmers.se>
2019-10-16 23:43:29 +0200

    Added controller, minor fix

---
+++ src/main/java/viewcontroller/views/dynamiccomponents/SpaceQuestionViewController.java
@@ -0,0 +1,36 @@
+package main.java.viewcontroller.views.dynamiccomponents;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import main.java.viewcontroller.views.MainViewController;
+import java.io.IOException;
+
+
+public class SpaceQuestionViewController extends AnchorPane {
+
+    MainViewController parentController;
+
+    @FXML
+    private TextField availableSpace;
+
+
+    public SpaceQuestionViewController(MainViewController parentController) {
+
+        this.parentController = parentController;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/dynamic/spacequestion.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        setLeftAnchor(this,0.0);
+        setRightAnchor(this,0.0);
+    }
+}
