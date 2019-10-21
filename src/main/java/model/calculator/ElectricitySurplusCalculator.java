f5db65e9646a1447db6668a4187ca7befe8b3624
Vegard <vegard.landro1@gmail.com>
2019-10-21 10:43:24 +0200

    Changes to ElectricitySurplusCalculator, works with new Facade

--- src/main/java/model/calculator/ElectricitySurplusCalculator.java
+++ src/main/java/model/calculator/ElectricitySurplusCalculator.java
@@ -4,38 +4,47 @@ package main.java.model.calculator;
 // Class for calculating surplus energy (consumed and produced). Unit of time to be specified
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 import static main.java.model.calculator.DataKey.*;
 
-class ElectricitySurplusCalculator implements Calculator{
+final class ElectricitySurplusCalculator implements Calculator{
 
     private final Set<DataKey> requiredInput = new HashSet<>(Arrays.asList(PRODUCED_ELECTRICITY, CONSUMED_ELECTRICITY));
+    private final Set<DataKey> output = new HashSet<>(Arrays.asList(SURPLUS));
+
 
     ElectricitySurplusCalculator() {
     }
 
     // Calculates and returns surplus
     @Override
-    public void calculate(CalculatorData input) {
-
+    public HashMap<DataKey, Double> calculate(HashMap<DataKey, Double> input) {
+        HashMap<DataKey, Double> data = new HashMap<>(input);
 
         double surplus = 0;
 
-        double produced = input.getValue(PRODUCED_ELECTRICITY);
-        double consumed = input.getValue(CONSUMED_ELECTRICITY);
+        double produced = input.get(PRODUCED_ELECTRICITY);
+        double consumed = input.get(CONSUMED_ELECTRICITY);
 
         //Checks if there is a surplus and calculates if there is, no negative surplus possible
         if(produced > consumed){
             surplus = produced - consumed;
         }
 
-        input.addValue(SURPLUS, surplus);
+        data.put(SURPLUS, surplus);
+        return data;
     }
 
     @Override
     public Set<DataKey> getRequiredInput() {
         return requiredInput;
+    }
+
+    @Override
+    public Set<DataKey> getOutput() {
+        return output;
     }
 }
