fda60ee5f8e37a8b953df23398e55fbcaf43a521
Vegard <vegard.landro1@gmail.com>
2019-10-21 10:43:05 +0200

    Changes to YearsToBreakEvenCalculator, works with new Facade

--- src/main/java/model/calculator/YearsToBreakEvenCalculator.java
+++ src/main/java/model/calculator/YearsToBreakEvenCalculator.java
@@ -3,34 +3,44 @@ package main.java.model.calculator;
 //(1.1) Vegard
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 import static main.java.model.calculator.DataKey.*;
 
-public class YearsToBreakEvenCalculator implements Calculator{
+final class YearsToBreakEvenCalculator implements Calculator{
 
     private final Set<DataKey> requiredInput = new HashSet<>(Arrays.asList(INSTALLATION_COST, ANNUAL_OPERATION_COST, ANNUAL_ELECTRICITY_PRODUCTION, IMPORTED_ELECTRICITY_PRICE_KWH));
+    private final Set<DataKey> output = new HashSet<>(Arrays.asList(YEARS_TO_BREAK_EVEN));
 
     YearsToBreakEvenCalculator() {
     }
 
     //Returns approximate time to break-even on costs for a electricity generation setup
-    public void calculate(CalculatorData input){
+    @Override
+    public HashMap<DataKey, Double> calculate(HashMap<DataKey, Double> input){
+        HashMap<DataKey, Double> data = new HashMap<>(input);
 
-        double installationCost         = input.getValue(INSTALLATION_COST);
-        double annualOperationCost      = input.getValue(ANNUAL_OPERATION_COST);
-        double annualProduction         = input.getValue(ANNUAL_ELECTRICITY_PRODUCTION);
-        double electricityPricePerKWh   = input.getValue(IMPORTED_ELECTRICITY_PRICE_KWH);
+        double installationCost         = input.get(INSTALLATION_COST);
+        double annualOperationCost      = input.get(ANNUAL_OPERATION_COST);
+        double annualProduction         = input.get(ANNUAL_ELECTRICITY_PRODUCTION);
+        double electricityPricePerKWh   = input.get(IMPORTED_ELECTRICITY_PRICE_KWH);
 
         double savingsPerYear = annualProduction * electricityPricePerKWh - annualOperationCost;
         double yearsToPayInstallationCost = installationCost/savingsPerYear;
 
-        input.addValue(YEARS_TO_BREAK_EVEN, yearsToPayInstallationCost);
+        data.put(YEARS_TO_BREAK_EVEN, yearsToPayInstallationCost);
+        return data;
     }
 
     @Override
     public Set<DataKey> getRequiredInput() {
         return requiredInput;
+    }
+
+    @Override
+    public Set<DataKey> getOutput() {
+        return output;
     }
 }
