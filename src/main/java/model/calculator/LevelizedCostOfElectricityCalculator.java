4e30c0a671a9ba8bbdf6856e2796b37a740265a6
Vegard <vegard.landro1@gmail.com>
2019-10-21 10:42:33 +0200

    Changes to LevelizedCostOfElectricityCalculator, works with new Facade

--- src/main/java/model/calculator/LevelizedCostOfElectricityCalculator.java
+++ src/main/java/model/calculator/LevelizedCostOfElectricityCalculator.java
@@ -3,36 +3,46 @@ package main.java.model.calculator;
 //(4.2) Vegard
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 import static main.java.model.calculator.DataKey.*;
 
-class LevelizedCostOfElectricityCalculator implements Calculator{
+final class LevelizedCostOfElectricityCalculator implements Calculator{
 
     private final Set<DataKey> requiredInput = new HashSet<>(Arrays.asList(EXPECTED_LIFESPAN, INSTALLATION_COST, ANNUAL_OPERATION_COST, ANNUAL_ELECTRICITY_PRODUCTION));
+    private final Set<DataKey> output = new HashSet<>(Arrays.asList(LEVELIZED_ELECTRICITY_COST));
 
     LevelizedCostOfElectricityCalculator() {
     }
 
     //Returns approximate cost of each kWh produced by a electricity generation setup
     @Override
-    public void calculate(CalculatorData input) {
+    public HashMap<DataKey, Double> calculate(HashMap<DataKey, Double> input) {
+        HashMap<DataKey, Double> data = new HashMap<>(input);
 
-        double lifetime             = input.getValue(EXPECTED_LIFESPAN);
-        double installationCost     = input.getValue(INSTALLATION_COST);
-        double annualOperationCost  = input.getValue(ANNUAL_OPERATION_COST);
-        double annualOutput         = input.getValue(ANNUAL_ELECTRICITY_PRODUCTION);
+
+        double lifetime             = input.get(EXPECTED_LIFESPAN);
+        double installationCost     = input.get(INSTALLATION_COST);
+        double annualOperationCost  = input.get(ANNUAL_OPERATION_COST);
+        double annualOutput         = input.get(ANNUAL_ELECTRICITY_PRODUCTION);
 
         double lifetimeOutput       = lifetime * annualOutput;
         double totalCost            = installationCost + (lifetime * annualOperationCost);
-        double levelizedCost        = lifetimeOutput / totalCost;
+        double levelizedCost        = totalCost / lifetimeOutput;
 
-        input.addValue(DataKey.LEVELIZED_ELECTRICITY_COST, levelizedCost);
+        data.put(LEVELIZED_ELECTRICITY_COST, levelizedCost);
+        return data;
     }
 
     @Override
     public Set<DataKey> getRequiredInput() {
         return requiredInput;
+    }
+
+    @Override
+    public Set<DataKey> getOutput() {
+        return output;
     }
 }
