d21854631daa0e9e3b5f5c62a1d2e09704e17f96
jutb <bunis@student.chalmers.se>
2019-10-22 16:38:30 +0200

    minor fix

--- src/main/java/viewcontroller/views/dynamiccomponents/FindLocationViewController.java
+++ src/main/java/viewcontroller/views/dynamiccomponents/FindLocationViewController.java
@@ -21,27 +21,25 @@ public class FindLocationViewController extends AnchorPane {
 
         this.parentController = parentController;
 
+        //Initialize this controller as a javafx node while coupling it with a fxml
         parentController.getPrimaryController().initDynamicComponent("/fxml/dynamic/locationfinder.fxml",this);
 
         //Match width to parent.
         setLeftAnchor(this, 0.0);
         setRightAnchor(this, 0.0);
 
+        //Shows updates location and shows location in gui
         findlocationButton.setOnAction(event -> {
             String city;
             city = parentController.getPrimaryController().getServiceFacade().getCity();
             cityText.setText(city);
 
-            System.out.println("one");
 
             double latitude = parentController.getPrimaryController().getServiceFacade().getLatitude();
             double longitude = parentController.getPrimaryController().getServiceFacade().getLongitude();
 
-            System.out.println("two");
+            parentController.getPrimaryController().getModelFacade().getLocation().setCoordinate(latitude, longitude);
 
-            parentController.getPrimaryController().getServiceFacade().getLocation().setCoordinate(latitude, longitude);
-
-            System.out.println("three");
         });
     }
 }
